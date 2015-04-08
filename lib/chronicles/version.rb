36c29c2333ca88fdce3de92bd5485137a129cc20
Andrew Kozin <andrew.kozin@gmail.com>
2015-04-08 13:57:10 +0300

    [VERSION] 0.0.1
    The first version (can prove unstable).

--- lib/chronicles/version.rb
+++ lib/chronicles/version.rb
@@ -4,6 +4,6 @@ module Chronicles
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "0.0.1-alpha".freeze
+  VERSION = "0.0.1".freeze
 
 end # module Chronicles
